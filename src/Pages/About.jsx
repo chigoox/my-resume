746656f0b4dfe025157082fb15829be27e114e8b
chigoox <dikeemmanuel54@gmail.com>
2023-03-19 01:37:54 -0400

    new

---
+++ src/Pages/About.jsx
@@ -0,0 +1,15 @@
+
+import React, { useEffect, useState } from 'react'
+import { handleInput5, addToDatabase } from '../MyCodes/ed5'
+import MenuButton from '../componets/MenuButton'
+
+
+function About() {
+
+    return (
+        <div className='bg-black h-full w-full relative overflow-y-scroll flex flex-col justify-center border-black border items-center'>
+        </div>
+    )
+}
+
+export default About
